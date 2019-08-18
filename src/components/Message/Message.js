cc6893b8158529f2ad68db48c003d4e26bc173d3
alexanderutrobin11 <alexanderutrobin11@gmail.com>
2019-08-18 19:41:34 +0300

    best code

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,10 +2,23 @@ import React from 'react';
 import './Message.css';
 
 function Message(props) {
+  const {type, value, inputHandler, index} = props;
+
+  let input = '';
+
+  if ((type === 'start' && index === 1)
+    || (type === 'roommates' && index === 1)
+    || (type === 'roommates' && index === 7)
+    || (type === 'roommates' && index === 8)
+  ) {
+    input = <input onChange={inputHandler} value={value} type="text"/>;
+  }
+
     return (
         <div className="Message">
             <div className="MessageText">
                 {props.children}
+                {input}
             </div>
         </div>
     );
--- src/components/View/View.js
+++ src/components/View/View.js
@@ -2,41 +2,135 @@ import React from 'react';
 import './View.css';
 import Message from '../Message/Message';
 import Button from "../Controls/Button/Button";
-import {text} from '../../data/Text';
+import {textHandler} from "../../data/Text";
 
 class View extends React.Component {
   state = {
-    messageData: text[0].messageData,
-    chooseData: text[0].chooseData,
-    type: text[0].type,
+    type: '',
     index: 0,
+    dataForCollect: [],
+    chooseData: [],
+    messageData: '',
+    questionsQuantity: 10,
+    inputs: [],
+    currentValue: '',
   };
 
-  nextHandler = () => {
-    const newIndex = this.state.index + 1;
+  updateStateHandler = (type, messageData, chooseData, index) => {
+    this.setState({
+      type,
+      messageData,
+      chooseData,
+      index,
+    });
+  };
+
+  inputHandler = (e) => {
+    this.setState({
+      currentValue: e.target.value,
+    });
+  };
+
+  componentDidMount() {
+    const {messageData, chooseData, type} = textHandler('start', 0, this.inputHandler, this.state.currentValue);
+    this.updateStateHandler(type, messageData, chooseData, 0);
+  }
+
+  nextHandler = (el) => {
+    if (el === 'Далее' && this.state.index === 0 && this.state.type === 'start') {
+      const {messageData, chooseData, type} = textHandler('start', 1, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 1);
+    }
+
+    if (el === 'Далее' && this.state.index === 1 && this.state.type === 'start') {
+      const {messageData, chooseData, type} = textHandler('roommates', 1, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 1);
+    }
+
+    // if (el === 'Ищу себе соседа') {
+    //   const {messageData, chooseData, type} = textHandler('roommates', 1);
+    //   this.updateStateHandler(type, messageData, chooseData, 1);
+    // }
+
+    if (el === 'Отправить' && this.state.type === 'roommates' && this.state.index === 1) {
+      const {messageData, chooseData, type} = textHandler('roommates', 2, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 2);
+    }
+
+    if (this.state.type === 'roommates' && this.state.index === 2) {
+      const {messageData, chooseData, type} = textHandler('roommates', 3, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 3);
+    }
+
+    if (this.state.type === 'roommates' && this.state.index === 3) {
+      const {messageData, chooseData, type} = textHandler('roommates', 4, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 4);
+    }
+
+    if (this.state.type === 'roommates' && this.state.index === 4) {
+      const {messageData, chooseData, type} = textHandler('roommates', 5, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 5);
+    }
+
+    if (this.state.type === 'roommates' && this.state.index === 5) {
+      const {messageData, chooseData, type} = textHandler('roommates', 6, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 6);
+    }
+
+    if (this.state.type === 'roommates' && this.state.index === 6) {
+      const {messageData, chooseData, type} = textHandler('roommates', 7, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 7);
+    }
+
+    if (this.state.type === 'roommates' && this.state.index === 7) {
+      const {messageData, chooseData, type} = textHandler('roommates', 8, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 8);
+    }
+
+    if (this.state.type === 'roommates' && this.state.index === 8) {
+      const {messageData, chooseData, type} = textHandler('last', 0, this.inputHandler, this.state.currentValue);
+      this.updateStateHandler(type, messageData, chooseData, 8);
+    }
+
+    if (this.state.currentValue) {
+      this.setState({
+        inputs: this.state.inputs.concat(this.state.currentValue),
+      })
+    }
 
     this.setState({
-      messageData: text[newIndex].messageData,
-      chooseData: text[newIndex].chooseData,
-      type: text[newIndex].type,
+      questionsQuantity: this.state.questionsQuantity - 1,
+      dataForCollect: this.state.dataForCollect.concat(el),
+      currentValue: '',
     });
   };
 
   render() {
-    const {messageData, chooseData, type} = this.state;
+    console.log(this.state);
+    const {messageData, chooseData} = this.state;
     let buttonsInChooseData = [];
 
     if (chooseData.length) {
-      buttonsInChooseData = chooseData.map(el => {
-        return <Button onClick={this.nextHandler} text={el} />
+      buttonsInChooseData = chooseData.map((el, index) => {
+        return <Button key={index} onClick={() => this.nextHandler(el)} text={el} />
       });
+    }
+
+    let buttons = <div className="ButtonsWrapper">{buttonsInChooseData}</div>;
+
+    if (buttonsInChooseData.length > 2) {
+      buttons = <div className="ButtonsWrapperMultiple">{buttonsInChooseData}</div>;
     }
 
     return (
       <div className="View">
-        <Message>{messageData}</Message>
-        {type === 'chooseFlow' && <div className="ButtonsWrapper">{buttonsInChooseData}</div>}
-        {type === 'start' && <Button onClick={this.nextHandler} text="Далее"/>}
+        {this.state.questionsQuantity ? <div className='QuestionsQuantity'>Осталось вопросов: {this.state.questionsQuantity}</div> : ''}
+        <Message
+          type={this.state.type}
+          index={this.state.index}
+          value={this.state.currentValue}
+          inputHandler={this.inputHandler}>{messageData}</Message>
+        {buttons}
       </div>
     );
   }
