5a2572171025e47f44b162a92bcaf2d36b138343
Arutyun Mnatsakanyan <mnatsakanayan@alor.ru>
2019-08-19 19:18:10 +0300

    Updates

---
+++ precache-manifest.7a47837768edf9d8759b22f03b01149d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf7c0632eadaed9d6bc69f99b2bd73d",
+    "url": "/room8/index.html"
+  },
+  {
+    "revision": "f42037345a36d021c376",
+    "url": "/room8/static/css/main.f914039e.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/room8/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "f42037345a36d021c376",
+    "url": "/room8/static/js/main.cc85e8b3.chunk.js"
+  },
+  {
+    "revision": "319a21e67b35f8733a77",
+    "url": "/room8/static/js/runtime~main.013cddc2.js"
+  }
+]);
