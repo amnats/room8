5a304f8b53cc5e19da931bb105dbc86f40b1e0ff
Arutyun Mnatsakanyan <mnatsakanayan@alor.ru>
2019-08-19 18:49:49 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/room8/precache-manifest.651af30351cc1f5316b39d12b9754c25.js"
+  "/precache-manifest.38fac4c62b391f3188227c245906f26d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/room8/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
