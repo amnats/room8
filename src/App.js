bb3aef31af8a8052aa49c7526f48f6cde904da9a
Arutyun Mnatsakanyan <mnatsakanayan@alor.ru>
2019-08-21 14:41:32 +0300

    send form results to firestore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import View from './components/View/View';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyC6Bmz27pWZ4q9dOv8i4AUllEY3sLL9Pm4',
+  authDomain: "room8-fd9e6.firebaseapp.com",
+  databaseURL: "https://room8-fd9e6.firebaseio.com",
+  projectId: "room8-fd9e6",
+});
+
+const db = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
-      <View/>
+      <View db={db}/>
     </div>
   );
 }
--- src/components/View/View.js
+++ src/components/View/View.js
@@ -3,7 +3,6 @@ import './View.css';
 import Message from '../Message/Message';
 import Button from "../Controls/Button/Button";
 import {textHandler} from "../../data/Text";
-import {sendEmail} from '../../services/emailSender.js';
 
 class View extends React.Component {
   state = {
@@ -91,7 +90,15 @@ class View extends React.Component {
     if (this.state.type === 'roommates' && this.state.index === 8) {
       const {messageData, chooseData, type} = textHandler('last', 0, this.inputHandler, this.state.currentValue);
       this.updateStateHandler(type, messageData, chooseData, 8);
-      sendEmail(this.state.dataForCollect);
+      
+      this.props.db.collection('profiles')
+        .add(JSON.parse(JSON.stringify(this.state)))
+        .then(function(docRef) {
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+          console.error("Error adding document: ", error);
+        });
     }
 
     if (this.state.currentValue) {
