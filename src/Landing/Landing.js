a0bb070a34c7375e30d004b5675fbfef5f13e383
alexanderutrobin11 <alexanderutrobin11@gmail.com>
2019-08-29 00:07:05 +0300

    landing before a tags

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -16,15 +16,18 @@ function Landing() {
           </div>
         </div>
       </div>
+
       <div className="LandingButtonBlock">
         <div className="LandingButton SText WhiteFont">
           Найти соседа
         </div>
       </div>
+
       <div className="LandingBenefitsBlock">
         <div className="LandingBenefitsTitle Bold MText">
           Преимущества
         </div>
+
         <div className="LandingBenefitsItem">
           <div className="LandingBenefitsItemLine"/>
           <div className="LandingBenefitsItemContent">
@@ -36,6 +39,47 @@ function Landing() {
             </div>
           </div>
         </div>
+
+        <div className="LandingBenefitsItem">
+          <div className="LandingBenefitsItemLine"/>
+          <div className="LandingBenefitsItemContent">
+            <div className="LandingBenefitsItemTitle Bold">
+              Найдите соседа мечты
+            </div>
+            <div className="LandingBenefitsItemDescription SText">
+              Ответьте на несколько вопросов и мы подберем наиболее подходящих кандидатов
+            </div>
+          </div>
+        </div>
+
+        <div className="LandingBenefitsItem">
+          <div className="LandingBenefitsItemLine"/>
+          <div className="LandingBenefitsItemContent">
+            <div className="LandingBenefitsItemTitle Bold">
+              Платите меньше за аренду
+            </div>
+            <div className="LandingBenefitsItemDescription SText">
+              Чем быстрее мы подберем вам соседа, тем скорее вы начнете делить с соседом плату за квартиру
+            </div>
+          </div>
+        </div>
+
+        <div className="LandingButtonBlock">
+          <div className="LandingButton SText WhiteFont">
+            Найти соседа
+          </div>
+        </div>
+
+        <div className="LandingContacts">
+          <div className="LandingContactsTitle Bold MText">
+            Контакты
+          </div>
+          <div className="LandingEmail SText">
+            room8.staff@inbox.ru
+          </div>
+        </div>
+
+
       </div>
     </>
   );
